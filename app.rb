68947dcfd4bff8dd727fa3e3faade2d784975c7b
daniellasheva <daniellasheva@gmail.com>
2016-08-12 13:27:47 -0400

    initial commit

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -11,6 +12,17 @@ class App < Sinatra::Base
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
+  end
+
+  get "/goodbye/:name" do
+    @name= params[:name]
+    "Goodbye #{@name}."
+  end
+
+  get "/multiply/:num1/:num2" do
+    @num1=params[:num1].to_i
+    @num2=params[:num2].to_i
+    (@num1 * @num2).to_s
   end
 
 
